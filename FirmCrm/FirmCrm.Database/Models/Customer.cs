c3d82854b12a193ad45ea1b33bc904f5f86b4d9a
eldje <ulrik.vandenbulcke@gmail.com>
2016-05-14 23:11:10 +0200

    models & dbcontext

---
+++ FirmCrm/FirmCrm.Database/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmCrm.Database.Models
+{
+    class Customer
+    {
+        public Customer(string lastName, string firstName, Address address)
+        {
+            this.LastName = LastName;
+            this.FirstName = FirstName;
+            this.Address = address;
+        }
+        public int CustomerId { get; set; }
+        public int CutomerNumber { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ FirmCrm/FirmCrm.Database/Models/Supplier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmCrm.Database.Models
+{
+    class Supplier
+    {
+        public int SupplierId { get; set; }
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+    }
+}
